456d0b37c0b73820c0e49a260ed498123a7293af
Jitto C Abraham <102623723+Jitto-resh@users.noreply.github.com>
2023-04-30 16:50:23 +0400

    rs to AED commit

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -12,7 +12,7 @@ const Details = ({ title }) => {
         <Card className={title === "Income" ? classes.income : classes.expense}>
             <CardHeader title={title} />
             <CardContent>
-                <Typography variant="h5">₹{total}</Typography>
+                <Typography variant="h5">AED{total}</Typography>
                 {/* {Object.keys(chartData).length !== 0 ? (<Doughnut data={chartData} />) : null} */}
                 <Doughnut data={chartData} />
             </CardContent>
